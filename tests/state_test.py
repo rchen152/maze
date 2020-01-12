a43e039ede0e7936859d6093183586444b7a465d
Rebecca Chen <rechen@google.com>
2020-01-11 21:08:35 -0800

    Add testing framework.

---
+++ tests/state_test.py
@@ -0,0 +1,14 @@
+"""Tests for maze.state."""
+
+import unittest
+
+
+# TODO(rchen152): Add real tests after creating helpers package.
+class ShortEscapeEndingTest(unittest.TestCase):
+
+    def test_hello_world(self):
+        print('hello!')
+
+
+if __name__ == '__main__':
+    unittest.main()

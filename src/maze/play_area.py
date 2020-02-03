f8f2dafdafc5566afe8f1c10a3487ff0a83da761
Rebecca Chen <rechen@google.com>
2020-02-03 09:40:59 -0800

    Improve player positioning after collision.

--- src/maze/play_area.py
+++ src/maze/play_area.py
@@ -2,7 +2,7 @@
 
 import pygame
 from pygame.locals import *
-from typing import Callable, Mapping, Optional, Union
+from typing import Callable, Mapping, NamedTuple, Optional, Tuple, Union
 
 from common import color
 from common import img
@@ -29,6 +29,25 @@ def _load(name, *args, **kwargs):
     return lambda screen: _MovablePngFactory(name, screen, *args, **kwargs)
 
 
+def _shift_speed(speed, direction):
+    return tuple(s + direction * _PLAYER_SPEED_INTERVAL * s / abs(s) if s else s
+                 for s in speed)
+
+
+def _decelerate(speed):
+    speed = _shift_speed(speed, -1)
+    return None if speed == (0, 0) else speed
+
+
+def _accelerate(speed):
+    return _shift_speed(speed, 1)
+
+
+class _Collision(NamedTuple):
+    max_nocollision_speed: Optional[Tuple[int, int]]
+    reason: str
+
+
 class Surface(objects.Surface):
     """A subsurface with movable objects on it."""
 
@@ -52,23 +71,33 @@ class Surface(objects.Surface):
         super().draw()
         self.player.draw()
 
-    def check_player_collision(self) -> Optional[str]:
+    def check_player_collision(self) -> Optional[_Collision]:
         # Check if the player's feet would hit anything if he took a step
         # against the background scroll direction.
         current_feet_rect = pygame.Rect(
             self.player.RECT.x, self.player.RECT.bottom - _PLAYER_FEET_HEIGHT,
             self.player.RECT.w, _PLAYER_FEET_HEIGHT)
-        next_feet_rect = current_feet_rect.move(
-            tuple(-s for s in self._scroll_speed))
-        player_path_rect = current_feet_rect.union(next_feet_rect)
+
+        def _get_player_path_rect(speed):
+            next_feet_rect = current_feet_rect.move(tuple(-s for s in speed))
+            return current_feet_rect.union(next_feet_rect)
+
+        player_path_rect = _get_player_path_rect(self._scroll_speed)
         for name, obj in self._objects.items():
             if player_path_rect.colliderect(obj.RECT):
+                # Find the maximum speed at which the player won't collide.
+                speed = _decelerate(self._scroll_speed)
+                while speed:
+                    if not _get_player_path_rect(speed).colliderect(obj.RECT):
+                        break
+                    speed = _decelerate(speed)
                 if name == 'house':
-                    return "You don't want to go back in the house."
+                    reason = "You don't want to go back in the house."
                 elif name.startswith('wall'):
-                    return "That's a wall..."
+                    reason = "That's a wall..."
                 else:
                     raise NotImplementedError(f'Collided with {name}')
+                return _Collision(speed, reason)
         return None
 
     def handle_player_movement(self, event) -> Union[bool, str]:
@@ -80,15 +109,22 @@ class Surface(objects.Surface):
             self._scroll_speed = _PLAYER_MOVES[event.key]
             pygame.time.set_timer(TICK, TICK_INTERVAL_MS)
         elif event.type == TICK:
-            assert self._scroll_speed  # for pytype
-            self._scroll_speed = tuple(
-                s + _PLAYER_SPEED_INTERVAL * s / abs(s) if s else s
-                for s in self._scroll_speed)
+            if not self._scroll_speed:
+                # The player has been stopped by an obstacle.
+                return True
+            self._scroll_speed = _accelerate(self._scroll_speed)
         else:
             return False
-        collision_reason = self.check_player_collision()
-        if collision_reason:
-            return collision_reason
-        for name, obj in self._objects.items():
-            obj.move(self._scroll_speed)
-        return True
+        collision = self.check_player_collision()
+        if collision:
+            # Move the player as close to the obstacle as possible.
+            speed = collision.max_nocollision_speed
+            move_result = collision.reason
+            self._scroll_speed = None
+        else:
+            speed = self._scroll_speed
+            move_result = True
+        if speed:
+            for name, obj in self._objects.items():
+                obj.move(speed)
+        return move_result
